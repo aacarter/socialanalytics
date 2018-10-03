3abf85aed6867255e82ecf1a54ab73ce5a0279fc
user <user@stp-er-10-129-120-73.wireless.umn.edu>
2018-10-02 20:08:32 -0500

    adding jupyter notebook

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "9S8jTc1e98Yt58dBtlWkQpunw"
+consumer_secret = "JJrTaIQDJJYRvpZAhGec0xXZx1O5GBb5Ao7e6iCMzyX2SLRqTH"
+access_token = "1036422227456143367-hxdYfCV8vXjnpLtOQnc4fc7BGjed9d"
+access_token_secret = "rniSYQQleHchmnzQMFi9wPYhopqLjqFm2o4rL2lFmphVM"
+weather_api_key = "5af051bf1c84d1e8c82a67ab3cea456b"
